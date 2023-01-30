a741cab2a1540110226b970ad7fc2132fe64e5f8
Emanoel Silva <emanoel.silva@grupofm.local>
2023-01-30 11:39:30 -0300

    Adicionando novos arquivos

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/auth/register', async(req, res) => {
     }
     
     if (password !== confirmpassword) {
-        return res.status(422).json({ msg: "As senhas não iguais." })
+        return res.status(422).json({ msg: "As senhas não são iguais." })
     }
 
     const userExists = await User.findOne({ email: email })
